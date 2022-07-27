9faed2d51727ed8c406252637048419f0cc6d3c0
Jon Morina <jonmorina23@gmail.com>
2022-07-27 13:21:50 +0200

    Test branch commit

--- BloodBankApp/Controllers/HomeController.cs
+++ BloodBankApp/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BloodBankApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        //test
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
