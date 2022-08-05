c5d199ad027ab7b192be2a55e0c68fb90994bb49
DoruntinaKorca <dk40651@ubt-uni.net>
2022-08-05 16:21:16 +0200

    Identity areas added

---
+++ BloodBankApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BloodBankApp.Data;
+using BloodBankApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BloodBankApp.Areas.Identity.IdentityHostingStartup))]
+namespace BloodBankApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
