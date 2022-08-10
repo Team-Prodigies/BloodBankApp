4381b44b499c2a1503cbb1cc704bdaaca73093c4
RinaKorca <rk48713@ubt-uni.net>
2022-08-10 15:53:52 +0200

    Fx

---
+++ BloodBankApp/Areas/SuperAdmin/Controllers/CitiesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodBankApp.Areas.SuperAdmin.Controllers
+{
+    [Area("SuperAdmin")]
+    public class CitiesController : Controller
+    {
+        public IActionResult Cities()
+        {
+            return View();
+        }
+    }
+}
