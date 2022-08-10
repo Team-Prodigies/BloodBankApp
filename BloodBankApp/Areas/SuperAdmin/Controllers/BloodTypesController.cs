905ba6c83309ad112ceb8298a5e72125dc63051f
Besmir Miftari <54418494+Besmirmiftari@users.noreply.github.com>
2022-08-10 10:23:24 +0200

    create blood types page and dislpay all blood types from database

---
+++ BloodBankApp/Areas/SuperAdmin/Controllers/BloodTypesController.cs
@@ -0,0 +1,24 @@
+﻿using BloodBankApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBankApp.Areas.SuperAdmin.Controllers {
+    [Area("SuperAdmin")]
+    public class BloodTypesController : Controller {
+
+        private readonly ApplicationDbContext context;
+
+        public BloodTypesController(ApplicationDbContext context) {
+            this.context = context;
+        }
+
+        public IActionResult BloodTypes() {
+
+            var bloodTypes = context.BloodTypes.ToList();
+            return View(bloodTypes);
+        }
+    }
+}
