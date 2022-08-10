0edc3120eca37c25711b6d26784aebf3a50475b6
Muhamet <muhametsadiku655@gmail.com>
2022-08-10 11:02:35 +0200

    Add new blood type funksion

--- BloodBankApp/Areas/SuperAdmin/Controllers/BloodTypesController.cs
+++ BloodBankApp/Areas/SuperAdmin/Controllers/BloodTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using BloodBankApp.Data;
+using BloodBankApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,21 @@ namespace BloodBankApp.Areas.SuperAdmin.Controllers {
 
         public BloodTypesController(ApplicationDbContext context) {
             this.context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddNewBloodType(string BloodTypeName) 
+        {
+            var bloodTypeExists = context.BloodTypes.Where(b => b.BloodTypeName.ToUpper() == BloodTypeName.ToUpper()).FirstOrDefault();
+            if (bloodTypeExists == null) 
+            {
+                BloodType newBloodType = new BloodType();
+                newBloodType.BloodTypeName = BloodTypeName;
+
+                context.Add(newBloodType);
+                context.SaveChanges();
+            }
+            return RedirectToAction("BloodTypes");
         }
 
         public IActionResult BloodTypes() {
