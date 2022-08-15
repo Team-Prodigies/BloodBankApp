09e9d579c77473b010d489f40354266ca6ccb8fc
Egzon Bllaca <egzonbllaca88@gmail.com>
2022-08-15 15:32:59 +0200

    Refactored the methods for getting chart data

--- BloodBankApp/Areas/SuperAdmin/Controllers/AdminHomeController.cs
+++ BloodBankApp/Areas/SuperAdmin/Controllers/AdminHomeController.cs
@@ -6,74 +6,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BloodBankApp.Areas.SuperAdmin.Controllers {
+namespace BloodBankApp.Areas.SuperAdmin.Controllers
+{
     [Area("SuperAdmin")]
-    public class AdminHomeController : Controller {
+    public class AdminHomeController : Controller
+    {
 
         private readonly ApplicationDbContext context;
-        public AdminHomeController(ApplicationDbContext context) {
+        public AdminHomeController(ApplicationDbContext context)
+        {
             this.context = context;
         }
 
-        public async Task<IActionResult> Index() {
-            
-            await GetUserRoleData();
+        public async Task<IActionResult> Index()
+        {
+
+            await GetUserRoleDataAsync();
             await GetUserBloodData();
             return View();
         }
 
-        private async Task GetUserRoleData()
+        private async Task GetUserRoleDataAsync()
         {
-            var getDonorRole = await  context.Roles.Where(x => x.Name == "Donor").FirstOrDefaultAsync();
-            int? donorCount = await context.UserRoles.Where(x => x.RoleId == getDonorRole.Id).CountAsync();
+            var data = new Dictionary<String, int>();
 
-            ViewBag.Donor = (donorCount == null) ? 0 : donorCount;
+            var userCountByRole =
+                from role in await context.Roles.ToListAsync()
+                join user in await context.UserRoles.ToListAsync() on role.Id equals user.RoleId into users
+                select new
+                {
+                    Role = role.Name,
+                    UserCount = users.Count()
+                };
 
-            var getSuperAdminRole = await context.Roles.Where(x => x.Name == "SuperAdmin").FirstOrDefaultAsync();
-            int? superAdminCount = await context.UserRoles.Where(x => x.RoleId == getSuperAdminRole.Id).CountAsync();
-
-            ViewBag.SuperAdmin = (superAdminCount == null) ? 0 : superAdminCount;
-
-            var getHospitalAdminRole = await context.Roles.Where(x => x.Name == "HospitalAdmin").FirstOrDefaultAsync();
-            int? hospitalAdminCount = await context.UserRoles.Where(x => x.RoleId == getHospitalAdminRole.Id).CountAsync();
-
-            ViewBag.HospitalAdmin = (hospitalAdminCount == null) ? 0 : hospitalAdminCount;
+            foreach (var entry in userCountByRole)
+            {
+                data.Add(entry.Role, entry.UserCount);
+            }
+            ViewData["UserRolesData"] = data;
         }
 
         private async Task GetUserBloodData()
         {
-            var getAPos = await context.BloodTypes.Where(x => x.BloodTypeName == "A+").FirstOrDefaultAsync();
-            int? aPosCount = await context.Donors.Where(x => x.BloodTypeId == getAPos.BloodTypeId).CountAsync();
-            ViewBag.aPositive= (aPosCount == null) ? 0 : aPosCount;
+            var data = new Dictionary<String, int>();
 
-            var getANeg = await context.BloodTypes.Where(x => x.BloodTypeName == "A-").FirstOrDefaultAsync();
-            int? aNegCount = await context.Donors.Where(x => x.BloodTypeId == getANeg.BloodTypeId).CountAsync();
-            ViewBag.aNegative = (aNegCount == null) ? 0 : aNegCount;
+            var donorCountByBloodType =
+                from bloodType in await context.BloodTypes.ToListAsync()
+                join donor in await context.Donors.ToListAsync() on bloodType.BloodTypeId equals donor.BloodTypeId into donors
+                select new
+                {
+                    BloodType = bloodType.BloodTypeName,
+                    DonorCount = donors.Count()
+                };
 
-            var getBPos = await context.BloodTypes.Where(x => x.BloodTypeName == "B+").FirstOrDefaultAsync();
-            int? bPosCount = await context.Donors.Where(x => x.BloodTypeId == getBPos.BloodTypeId).CountAsync();
-            ViewBag.bPositive = (bPosCount == null) ? 0 : bPosCount;
-
-            var getBNeg = await context.BloodTypes.Where(x => x.BloodTypeName == "B-").FirstOrDefaultAsync();
-            int? bNegCount = await context.Donors.Where(x => x.BloodTypeId == getBNeg.BloodTypeId).CountAsync();
-            ViewBag.bNegative = (bNegCount == null) ? 0 : bNegCount;
-
-
-            var getABPos = await context.BloodTypes.Where(x => x.BloodTypeName == "AB+").FirstOrDefaultAsync();
-            int? aBPosCount = await context.Donors.Where(x => x.BloodTypeId == getABPos.BloodTypeId).CountAsync();
-            ViewBag.aBPositive = (aBPosCount == null) ? 0 : aBPosCount;
-
-            var getABNeg = await context.BloodTypes.Where(x => x.BloodTypeName == "AB-").FirstOrDefaultAsync();
-            int? aBNegCount = await context.Donors.Where(x => x.BloodTypeId == getABNeg.BloodTypeId).CountAsync();
-            ViewBag.aBNegative = (aBNegCount == null) ? 0 : aBNegCount;
-
-            var getOPos = await context.BloodTypes.Where(x => x.BloodTypeName == "O+").FirstOrDefaultAsync();
-            int? oPosCount = await context.Donors.Where(x => x.BloodTypeId == getOPos.BloodTypeId).CountAsync();
-            ViewBag.oPositive = (oPosCount == null) ? 0 : oPosCount;
-
-            var getONeg = await context.BloodTypes.Where(x => x.BloodTypeName == "O-").FirstOrDefaultAsync();
-            int? oNegCount = await context.Donors.Where(x => x.BloodTypeId == getONeg.BloodTypeId).CountAsync();
-            ViewBag.oNegative = (oNegCount == null) ? 0 : oNegCount;
+            foreach (var entry in donorCountByBloodType)
+            {
+                data.Add(entry.BloodType, entry.DonorCount);
+            }
+            ViewData["BloodTypeData"] = data;
         }
 
         public IActionResult ManageCities()
