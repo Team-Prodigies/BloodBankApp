c8cf2301bc5215bbd2e8b3c7afe226f430def415
DoruntinaKorca <dk40651@ubt-uni.net>
2022-08-16 17:19:03 +0200

    Created UserPage & added functionality

---
+++ BloodBankApp/Areas/SuperAdmin/Controllers/DonorsController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using BloodBankApp.Areas.SuperAdmin.ViewModels;
+using BloodBankApp.Data;
+using BloodBankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBankApp.Areas.SuperAdmin.Controllers
+{
+    [Area("SuperAdmin")]
+    public class DonorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DonorsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Donors()
+        {
+            var users = await _context.Donors
+                .Select(d => new Donor
+                {
+                    DonorId = d.DonorId,
+                    User = d.User,
+                    PersonalNumber = d.PersonalNumber,
+                    Gender = d.Gender,
+                    BloodType = d.BloodType,
+                    City = d.City
+                }).ToListAsync();
+
+            var result = _mapper.Map<List<DonorModel>>(users);
+
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> LockoutDonor(Guid donorId)
+        {
+            var donor = await _context.Users.FindAsync(donorId);
+            if(donor != null)
+            {
+                donor.LockoutEnabled = true;
+                donor.LockoutEnd = DateTime.Now.AddMinutes(10); // DateTime.MaxValue if we want to lock a user forever
+                _context.Update(donor);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Donors");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DonorLockout(Guid donorId)
+        {
+            var donorLockout =await _context.Donors.FindAsync(donorId);
+
+            if (donorLockout == null)
+            {
+                return RedirectToAction("Donors");
+            }
+            return View(donorLockout);
+        }
+
+
+    }
+}
--- BloodBankApp/Mapping/MappingProfile.cs
+++ BloodBankApp/Mapping/MappingProfile.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using static BloodBankApp.Areas.Identity.Pages.Account.RegisterModel;
 
-namespace BloodBankApp.Mapping {
+namespace BloodBankApp.Mapping
+{
     public class MappingProfile : Profile
     {
 
@@ -18,12 +19,24 @@ namespace BloodBankApp.Mapping {
                 opts => opts.MapFrom(src => src.DateOfBirth));
 
             CreateMap<RegisterInputModel, Donor>()
-                .ForMember(dest=>dest.CityId,
-                opts=>opts.MapFrom(src=>src.CityId))
+                .ForMember(dest => dest.CityId,
+                opts => opts.MapFrom(src => src.CityId))
                 .ForMember(dest => dest.BloodTypeId,
                 opts => opts.MapFrom(src => src.BloodTypeId));
 
             CreateMap<SuperAdminModel, User>();
+
+            CreateMap<Donor, DonorModel>()
+                .ForPath(dest => dest.Name,
+                opts => opts.MapFrom(src => src.User.Name))
+                .ForPath(dest => dest.Surname,
+                opts => opts.MapFrom(src => src.User.Surname))
+                .ForPath(dest => dest.DateOfBirth,
+                opts => opts.MapFrom(src => src.User.DateOfBirth))
+                .ForPath(dest => dest.BloodTypeName,
+                opts => opts.MapFrom(src => src.BloodType.BloodTypeName))
+                .ForPath(dest => dest.CityName,
+                opts => opts.MapFrom(src => src.City.CityName));
         }
     }
 }
