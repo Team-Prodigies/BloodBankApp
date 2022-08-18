e7c75e81e43fce5df04ef5d703f572ae9ab62eff
Besmir Miftari <54418494+Besmirmiftari@users.noreply.github.com>
2022-08-18 13:12:21 +0200

    added filter and pagination to donors table page

--- BloodBankApp/Areas/SuperAdmin/Controllers/DonorsController.cs
+++ BloodBankApp/Areas/SuperAdmin/Controllers/DonorsController.cs
@@ -23,20 +23,34 @@ namespace BloodBankApp.Areas.SuperAdmin.Controllers
             _mapper = mapper;
         }
         [HttpGet]
-        public async Task<IActionResult> Donors()
+        public async Task<IActionResult> Donors(int pageNumber = 1, string filterBy = "A-Z")
         {
-            var users = await _context.Donors
-                .Select(d => new Donor
-                {
-                    DonorId = d.DonorId,
-                    User = d.User,
-                    PersonalNumber = d.PersonalNumber,
-                    Gender = d.Gender,
-                    BloodType = d.BloodType,
-                    City = d.City
-                }).ToListAsync();
+            var skipRows = (pageNumber - 1) * 10;
+            var users = new List<Donor>();
 
+            switch (filterBy)
+            {
+                case "A-Z":
+                    users = await _context.Donors.Include(user => user.User).Include(blood => blood.BloodType).Include(city => city.City).OrderBy(donor => donor.User.Name).Skip(skipRows).Take(10).ToListAsync();
+                    break;
+
+                case "Z-A":
+                    users = await _context.Donors.Include(user => user.User).Include(blood => blood.BloodType).Include(city => city.City).OrderByDescending(donor => donor.User.Name).Skip(skipRows).Take(10).ToListAsync();
+                    break;
+
+                case "Locked":
+                    users = await _context.Donors.Include(user => user.User).Include(blood => blood.BloodType).Include(city => city.City).Where(donor => donor.User.Locked == true).Skip(skipRows).Take(10).ToListAsync();
+                    break;
+
+                default:
+                    users = await _context.Donors.Include(user => user.User).Include(blood => blood.BloodType).Include(city => city.City).OrderBy(donor => donor.User.Name).Skip(skipRows).Take(10).ToListAsync();
+                    break;
+            }
+            
             var result = _mapper.Map<List<DonorModel>>(users);
+
+            ViewBag.FilterBy = filterBy;
+            ViewBag.PageNumber = pageNumber;
 
             return View(result);
         }
