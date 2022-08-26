4a287f883f447b4d1b137eb8de11b7cad297c48f
Egzon Bllaca <egzonbllaca88@gmail.com>
2022-08-26 16:02:31 +0200

    Added generic services

---
+++ BloodBankApp/Areas/Services/BaseService.cs
@@ -0,0 +1,54 @@
+﻿using BloodBankApp.Areas.Services.Interfaces;
+using BloodBankApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BloodBankApp.Areas.Services
+{
+    public class BaseService<T> : IBaseService<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+             _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> FindById(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<List<T>> GetByCondition(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>().Where(expression).AsNoTracking().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BloodBankApp/Areas/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BloodBankApp.Areas.Services.Interfaces
+{
+    public interface IBaseService<T>
+    {
+        Task<List<T>> GetAll();
+        Task<List<T>> GetByCondition(Expression<Func<T, bool>> expression);
+        Task<T> FindById(Guid id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
