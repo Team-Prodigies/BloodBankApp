750ed8ab488067d4952389f08817ab2c9eb96b3b
Muhamet <muhametsadiku655@gmail.com>
2022-09-14 16:34:40 +0200

    Create Donators page

---
+++ BloodBankApp/Areas/HospitalAdmin/Controllers/DonatorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace BloodBankApp.Areas.HospitalAdmin.Controllers
+{
+    [Area("HospitalAdmin")]
+    [Authorize(Roles = "HospitalAdmin")]
+    public class DonatorsController : Controller
+    {
+        public IActionResult ManageDonators()
+        {
+            return View();
+        }
+    }
+}
