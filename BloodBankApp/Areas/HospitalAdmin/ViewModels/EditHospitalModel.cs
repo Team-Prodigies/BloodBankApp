ab44c463955d33b80f43ca502d8c24bf2fcdf435
DoruntinaKorca <dk40651@ubt-uni.net>
2022-10-04 14:37:55 +0200

    Refactored Editing a Hospital on HospitalAdmin dashboard

--- BloodBankApp/Areas/HospitalAdmin/ViewModels/EditHospitalModel.cs
+++ BloodBankApp/Areas/HospitalAdmin/ViewModels/EditHospitalModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using BloodBankApp.Models;
 
 namespace BloodBankApp.Areas.HospitalAdmin.ViewModels
@@ -6,15 +7,16 @@ namespace BloodBankApp.Areas.HospitalAdmin.ViewModels
     public class EditHospitalModel
     {
         public Guid HospitalId { get; set; }
-
+        [Display(Name = "Hospital Name")]
         public string HospitalName { get; set; }
-        
+        [Display(Name = "Contact Number")]
         public string ContactNumber { get; set; }
 
         public string? HospitalCode { get; set; }
-
+        [Display(Name= "Location" )]
         public Guid LocationId { get; set; }
         public Location Location { get; set; }
+        [Display(Name = "City")]
         public Guid CityId { get; set; }
         public City City { get; set; }
     }
